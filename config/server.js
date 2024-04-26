a405013307982b815e319cf2e2f537159e480da7
talabidele <talabibamidele7@gmail.com>
2024-04-26 02:27:41 +0100

    Removed url from server.js

--- config/server.js
+++ config/server.js
@@ -4,5 +4,5 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: "https://motrac-backend-production.up.railway.app",
+  // url: "https://motrac-backend-production.up.railway.app",
 });
