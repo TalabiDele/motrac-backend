520532316b6becaa5ede99f772318b307bbd7130
talabidele <talabibamidele7@gmail.com>
2024-04-26 02:10:48 +0100

    Changed database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("POSTGRES_USER", "strapi"),
+      password: env("POSTGRES_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
