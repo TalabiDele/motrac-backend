91544130e291c6fe8b226e2c04e1c6b82ac028e2
talabidele <talabibamidele7@gmail.com>
2024-04-26 01:35:56 +0100

    Added db url to app

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,9 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } =
+    parse(env("DATABASE_URL")) ||
+    "https://motrac-backend-production.up.railway.app/";
 
   return {
     connection: {
