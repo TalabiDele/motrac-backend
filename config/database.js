bd24c9298218d3e1a078aa74915be72e85af9936
talabidele <talabibamidele7@gmail.com>
2024-04-29 02:29:14 +0100

    Bug fix #1

--- config/database.js
+++ config/database.js
@@ -14,11 +14,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5432),
+      host: env("HOST", "localhost"),
+      port: env.int("PORT", 1337),
       database: env("PGDATABASE", "strapi"),
-      user: env("POSTGRES_USER", "strapi"),
-      password: env("POSTGRES_PASSWORD", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "strapi"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,14 +8,14 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env("PGHOST", "127.0.0.1"),
-        port: env.int("PGPORT", 5432),
+        host: env("HOST", "localhost"),
+        port: env.int("PORT", 1337),
         database: env("PGDATABASE", "strapi"),
         user: env("PGUSER", "strapi"),
         password: env("PGPASSWORD", "password"),
         ssl: env.bool(true),
       },
-      pool: { min: 0 },
+      // pool: { min: 0 },
       // connection: {
       //   host,
       //   port,
