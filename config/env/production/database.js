9544166ea5a428a5b7378f69ddb87da587a05ed2
talabidele <talabibamidele7@gmail.com>
2024-04-29 13:38:38 +0100

    Updated files

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,33 +1,41 @@
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } =
-    "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway";
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } =
+//     "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway";
 
-  // "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway"
+//   // "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway"
 
-  console.log(host, port, database, user, password);
+//   console.log(host, port, database, user, password);
 
-  return {
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host: env("PGHOST", "127.0.0.1"),
+//         port: env.int("PGPORT", 5432),
+//         database: env("PGDATABASE", "strapi"),
+//         user: env("PGUSER", "strapi"),
+//         password: env("PGPASSWORD", "password"),
+//         ssl: env.bool(true),
+//       },
+//       pool: { min: 0 },
+//       // connection: {
+//       //   host,
+//       //   port,
+//       //   database,
+//       //   user,
+//       //   password,
+//       // },
+//       debug: false,
+//     },
+//   };
+// };
+
+module.exports = ({ env }) => ({
+  connection: {
     connection: {
-      client: "postgres",
-      connection: {
-        host: env("PGHOST", "127.0.0.1"),
-        port: env.int("PGPORT", 5432),
-        database: env("PGDATABASE", "strapi"),
-        user: env("PGUSER", "strapi"),
-        password: env("PGPASSWORD", "password"),
-        ssl: env.bool(true),
-      },
-      pool: { min: 0 },
-      // connection: {
-      //   host,
-      //   port,
-      //   database,
-      //   user,
-      //   password,
-      // },
-      debug: false,
+      connectionString: env("DATABASE_URL"),
     },
-  };
-};
+  },
+});
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
-  url: "https://motrac-backend-production.up.railway.app",
+  // url: "https://motrac-backend-production.up.railway.app",
+  url: "http://127.0.0.1:1337",
   // url: env("RENDER_EXTERNAL_URL"),
 });
