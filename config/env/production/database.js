dd4709e2bba4476cd3f6ae3fff835bcd27dd1c5f
talabidele <talabibamidele7@gmail.com>
2024-04-29 12:18:03 +0100

    PG host change

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => {
     connection: {
       client: "postgres",
       connection: {
-        host: env("HOST", "localhost"),
-        port: env.int("PORT", 1337),
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 5432),
         database: env("PGDATABASE", "strapi"),
         user: env("PGUSER", "strapi"),
         password: env("PGPASSWORD", "password"),
