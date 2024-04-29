357ca002c45f0f2c3cc76283e589d1389ccfd625
talabidele <talabibamidele7@gmail.com>
2024-04-29 13:26:23 +0100

    Bug fix #2

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "sqlite",
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: path.join(
+//         __dirname,
+//         "..",
+//         env("DATABASE_FILENAME", ".tmp/data.db")
+//       ),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } =
+    "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway";
+
+  // "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway"
+
+  return {
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
+      client: "postgres",
+      connection: {
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 5432),
+        database: env("PGDATABASE", "strapi"),
+        user: env("PGUSER", "strapi"),
+        password: env("PGPASSWORD", "password"),
+        ssl: env.bool(true),
+      },
+      pool: { min: 0 },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,25 +6,27 @@ module.exports = ({ env }) => {
 
   // "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway"
 
+  console.log(host, port, database, user, password);
+
   return {
     connection: {
       client: "postgres",
-      // connection: {
-      //   host: env("PGHOST", "127.0.0.1"),
-      //   port: env.int("PGPORT", 5432),
-      //   database: env("PGDATABASE", "strapi"),
-      //   user: env("PGUSER", "strapi"),
-      //   password: env("PGPASSWORD", "password"),
-      //   ssl: env.bool(true),
-      // },
-      // pool: { min: 0 },
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("PGHOST", "127.0.0.1"),
+        port: env.int("PGPORT", 5432),
+        database: env("PGDATABASE", "strapi"),
+        user: env("PGUSER", "strapi"),
+        password: env("PGPASSWORD", "password"),
+        ssl: env.bool(true),
       },
+      pool: { min: 0 },
+      // connection: {
+      //   host,
+      //   port,
+      //   database,
+      //   user,
+      //   password,
+      // },
       debug: false,
     },
   };
