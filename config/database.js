ea8fdf9c1fbec6bf0b7d58b8b4fb58f43fa2d327
talabidele <talabibamidele7@gmail.com>
2024-04-29 12:01:46 +0100

    Removed changes

--- config/database.js
+++ config/database.js
@@ -1,29 +1,15 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("HOST", "localhost"),
-      port: env.int("PORT", 1337),
-      database: env("PGDATABASE", "strapi"),
-      user: env("PGUSER", "strapi"),
-      password: env("PGPASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-      },
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  url: "https://motrac-backend-production.up.railway.app",
-  // url: env("RENDER_EXTERNAL_URL"),
+  // url: "https://motrac-backend-production.up.railway.app",
+  url: env("RENDER_EXTERNAL_URL"),
 });
