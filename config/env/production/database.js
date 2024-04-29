1fa6d81e479324e980f4ddae2f2597671e703d31
talabidele <talabibamidele7@gmail.com>
2024-04-29 12:21:19 +0100

    Reversed version

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,27 +2,29 @@ const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } =
-    "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@:/railway";
+    "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway";
+
+  // "postgresql://postgres:3CFbB2f6eFD1ae3FaDC-2eEbG5DFeDbC@roundhouse.proxy.rlwy.net:26838/railway"
 
   return {
     connection: {
       client: "postgres",
-      connection: {
-        host: env("PGHOST", "127.0.0.1"),
-        port: env.int("PGPORT", 5432),
-        database: env("PGDATABASE", "strapi"),
-        user: env("PGUSER", "strapi"),
-        password: env("PGPASSWORD", "password"),
-        ssl: env.bool(true),
-      },
-      // pool: { min: 0 },
       // connection: {
-      //   host,
-      //   port,
-      //   database,
-      //   user,
-      //   password,
+      //   host: env("PGHOST", "127.0.0.1"),
+      //   port: env.int("PGPORT", 5432),
+      //   database: env("PGDATABASE", "strapi"),
+      //   user: env("PGUSER", "strapi"),
+      //   password: env("PGPASSWORD", "password"),
+      //   ssl: env.bool(true),
       // },
+      // pool: { min: 0 },
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+      },
       debug: false,
     },
   };
